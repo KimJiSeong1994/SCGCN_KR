5b7b55e5edba7f2ecd2558b87fd75f326c4d90d9
Jiseong <js_2109@naver.com>
2023-09-15 16:48:27 +0900

    Edit GetData function -- 23. 09. 15

--- Reproduce/src/GetData.py
+++ Reproduce/src/GetData.py
@@ -1,18 +1,22 @@
 class Get_geoinfo :
     def __init__(self, place_nm : list) :
+        import itertools
         import osmnx as ox
+        import pandas as pd
         import geopandas as gpd
 
         self.area = ox.geocode_to_gdf(place_nm, which_result = 1)
-        buildings = ox.geometries_from_place(place_nm, {"building" : True})
-        buildings = buildings.loc[buildings.geometry.type == 'Polygon'] # Remove incomplete polygon
-        self.buildings = gpd.GeoDataFrame(buildings, geometry = 'geometry', crs = 'EPSG:4326')
-        self._get = {'area' : self.area, 'buildings' : self.buildings}
+        use_index = ['residential', 'commercial', 'industrial', 'retail']
+        use_sub_index =  ['greenfield', 'forest', 'grass', 'meadow', 'recreation_ground', 'cemetery']
+        use_leisure = ['park']
+
+        landuse = ox.geometries_from_place(place_nm, {"landuse" : True})
+        landuse = pd.concat([landuse[landuse["landuse"].isin(list(itertools.chain(*[use_index, use_sub_index])))], landuse[landuse["leisure"].isin(use_leisure)]])
+        landuse.loc[(landuse["landuse"].isin(use_sub_index)) | (landuse["leisure"].isin(use_leisure)), "landuse"] = "OpenSpace"
+        landuse = gpd.GeoDataFrame(landuse, geometry = "geometry", crs = 'EPSG:4326')
+
+        self.landuse = landuse[landuse.geometry.type == 'Polygon'] # Remove incomplete polygon
+        self._get = {'area' : self.area, 'landuses' : self.landuses}
 
     def __getitem__(self, item) :
         return self._get[item]
-
-if __name__ == "__main__" :
-    place_nm = ["Wuhan", "Hanyang", "Wuchang"] # "Jianghan"
-    info = Get_geoinfo(place_nm)
-    area, buildings = info['area'], info['buildings']
