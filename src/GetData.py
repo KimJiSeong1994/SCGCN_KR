b66a63767588d50146982f49d4e1b935e7d6e278
KimJiSeong1994 <js_2109@naver.com>
2023-09-28 22:03:21 +0900

    Add ResNet backbone -- 23. 09. 28

---
+++ src/GetData.py
@@ -0,0 +1,42 @@
+class Get_geoinfo :
+    def __init__(self, place_nm : list) :
+        import itertools
+        import osmnx as ox
+        import pandas as pd
+        import geopandas as gpd
+
+        self.area = ox.geocode_to_gdf(place_nm, which_result = 1)
+        use_index = ['residential', 'commercial', 'industrial', 'retail']
+        use_sub_index =  ['greenfield', 'forest', 'grass', 'meadow', 'recreation_ground', 'cemetery']
+        use_leisure = ['park']
+
+        landuse = ox.geometries_from_place(place_nm, {"landuse" : True})
+        landuse = pd.concat([landuse[landuse["landuse"].isin(list(itertools.chain(*[use_index, use_sub_index])))], landuse[landuse["leisure"].isin(use_leisure)]])
+        landuse.loc[(landuse["landuse"].isin(use_sub_index)) | (landuse["leisure"].isin(use_leisure)), "landuse"] = "OpenSpace"
+        landuse = gpd.GeoDataFrame(landuse, geometry = "geometry", crs = 'EPSG:4326')
+
+        self.landuse = landuse[landuse.geometry.type == 'Polygon'] # Remove incomplete polygon
+        self._get = {'area' : self.area, 'landuses' : self.landuse}
+
+    def __getitem__(self, item) :
+        return self._get[item]
+
+def Get_streetview(locations) :
+    import io
+    import requests
+    from PIL import Image
+    from src.config import config
+    param = {'fov' : '120', 'heading' : '-45', 'pitch' : '30', 'locations' : f'{locations}'}
+    url = f"https://maps.googleapis.com/maps/api/streetview?size=400x300&location={param['locations']}&fov={param['fov']}&heading={param['heading']}&pitch={param['pitch']}&key={config.GOOGLEMAP_API}"
+    Image.open(io.BytesIO(requests.request("GET", url, headers ={}, data = {}).content)).save(f"{config.FILE_PATH}/street_view{locations}.png")
+
+if __name__ == "__main__" :
+    from tqdm import tqdm
+    place_nm = ["seoul"]
+    info = Get_geoinfo(place_nm)
+    seoul_info = info["landuses"]
+    seoul_info.to_pickle("./Reproduce/data/seoul_landuse.pkl")
+
+    points = [[s.y, s.x] for _, s in enumerate(seoul_info.geometry.centroid.reset_index(drop = True))]
+    points = [str(p[0]) + "," + str(p[1]) for p in points]
+    _ = [Get_streetview(p) for p in tqdm(points)]
---
+++ src/utils.py
@@ -0,0 +1,16 @@
+class Utils :
+    @staticmethod
+    def Delaunay(points) :
+        import numpy as np
+        from tqdm import tqdm
+        from scipy.spatial import Delaunay
+
+        # points = [[s.x, s.y] for _, s in enumerate(landuse.geometry.centroid.reset_index(drop = True))]
+        tri = Delaunay(points)
+        PPE_ajd = np.zeros((len(points), len(points)))
+        for i in tqdm(tri.simplices.tolist()) :
+            PPE_ajd[i[0], i[1]] = 1
+            PPE_ajd[i[0], i[2]] = 1
+            PPE_ajd[i[1], i[2]] = 1
+
+        return PPE_ajd
