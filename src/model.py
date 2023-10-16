d48b48eaa985a078e2c1b08972ec94bbc1235848
KimJiSeong1994 <js_2109@naver.com>
2023-10-16 23:53:19 +0900

    Spatial context graph building class -- 23. 10. 16

--- src/model.py
+++ src/model.py
@@ -15,3 +15,4 @@ if __name__ == '__main__' :
     model.fc = nn.Sequential(nn.Linear(2048, 200), nn.ReLU(), nn.Linear(200, 5))  # model custom
     model.load_state_dict(torch.load('./src/SpatialContext/model_output/backbone_result.pth', map_location = torch.device('cpu')))
 
+
